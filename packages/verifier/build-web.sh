9c5dc96afa79df35fb242e94d27af3d61db437b4
Tao Tao <tao.tao@circle.com>
2024-07-29 10:41:48 -0700

    Change Circle Internet Financial, LTD. to Circle Internet Group, Inc. in the license line.

--- packages/verifier/build-web.sh
+++ packages/verifier/build-web.sh
@@ -1,4 +1,4 @@
-# Copyright 2024 Circle Internet Financial, LTD.  All rights reserved.
+# Copyright 2024 Circle Internet Group, Inc.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
